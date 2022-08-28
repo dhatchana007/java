c27e234af4e446d921c81695d44ebc2bb44e14cd
dhatchana007 <109939323+dhatchana007@users.noreply.github.com>
2022-08-28 15:58:00 +0530

    Create While.java

---
+++ While.java
@@ -0,0 +1,21 @@
+  while-entry level programe
+  programe first will be check condition after executed
+
+1.check 2 4 12 20 30
+    public class while
+    {
+      public static void main(String[] args)
+      {
+       
+        int i=1;
+        while(i<=2)
+        {
+          System.out.println(i*(i+1)+"");
+           i=i+1;
+        }
+      }
+    }
+   
+output
+2 4 12 20 30
+  
