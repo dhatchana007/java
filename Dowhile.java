b53b74eae3b4e8e75b6e6ec4225cfef4adbef26b
dhatchana007 <109939323+dhatchana007@users.noreply.github.com>
2022-08-28 15:26:58 +0530

    Create Dowhile.java

---
+++ Dowhile.java
@@ -0,0 +1,37 @@
+do while program
+
+
+//do while is a exit level programe
+//do is an first programe will be executed after check condition.
+
+//program to print no 1 to 5
+public class Dowhile
+{
+public static void main(String[] args)
+{
+int no=5;
+int i=1;
+do
+{
+System.out.println(i+"");
+i=i+1;
+}while(i<=5);
+}
+}
+
+2.program to reverse method
+public class Main
+{
+public static void main(String[] args)
+{
+int no=100;
+int i=1;
+do
+{
+System.out.println(i+"");
+i=i-2;
+}while(i>=1);
+}
+}
+
+
