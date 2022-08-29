fd263356ba8b4a2e8ce8e9650296f4689f6c80eb
dhatchana007 <109939323+dhatchana007@users.noreply.github.com>
2022-08-29 10:05:23 +0530

    Create primitive and nonprimitive.java

---
+++ DataTypes/primitive and nonprimitive.java
@@ -0,0 +1,7 @@
+Java is a statically-typed programming language. It means, all variables must be declared before its use. That is why we need to declare variable's type and name.
+In Java language, primitive data types are the building blocks of data manipulation. These are the most basic data types available in Java language.
+  there  are two types of data types in java
+1.primitive data types
+int,float,char,boolean,long,short,byte,char
+2.non-primitive data types
+string,class,interface,arrays
