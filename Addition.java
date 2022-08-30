65b72fc911222957a992e1b421ebee834427bace
dhatchana007 <109939323+dhatchana007@users.noreply.github.com>
2022-08-30 06:46:18 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,17 @@
+Addition of 4 numbers using dowhile
+
+class Addition
+{
+public static void main(String[] args)
+{
+int total=0;
+do
+{
+int no=10;
+total=total+10;
+no++;
+}
+while(total!=100);
+System.out.println(total);
+}
+}
