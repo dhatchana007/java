cd5a1a01f9e73d3b8ebf5effc8501c3e4415bdfc
dhatchana007 <109939323+dhatchana007@users.noreply.github.com>
2022-08-30 06:53:35 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -15,3 +15,19 @@ while(total!=100);
 System.out.println(total);
 }
 }
+
+Addition of first 10 number
+2.class Addition 
+{
+public static void main(String[] args)
+{
+int total=0;
+int i=1;
+while(no<=10)
+{
+total=total+i;
+i++;
+}
+System.out.println(total);
+}
+}
