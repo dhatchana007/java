8b537fa685ca8f597296e3497b2eff1ffe164d09
dhatchana007 <109939323+dhatchana007@users.noreply.github.com>
2022-09-03 13:59:37 +0530

    Update typecasting.java

--- typecasting.java
+++ typecasting.java
@@ -1,2 +1,24 @@
 Typecasting 
-changing one data type to another one
+changing one data type to another one data type
+
+class Typecasting
+{
+  Public static void main(String[] args)
+  {
+    int p=100;
+    float f=((float)p);
+    short s=((short)p);
+    long l=((long)p);
+    double d=((double)p);
+    
+    System.out.println(f);
+    System.out.println(s);
+    System.out.println(l);
+    System.out.println(d);
+    
+    output
+      100.0
+      100
+      100
+      100.0
+    
